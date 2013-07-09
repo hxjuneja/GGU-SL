fba5aa8f20092cb85cf5f724c707d9fa222d42ef
hardikj <hardikjuneja.hj@gmail.com>
2013-07-09 16:20:12 +0530

    A wotking model :)

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config(object):
+
+    def __init__(self):
+        self.username = "hardikj" #Your Github Username
+        self.pw = "" #Your Shell Password   
+ 
+    def get_username(self):
+        return self.username
+
+    def get_pw(self):
+        return self.pw
